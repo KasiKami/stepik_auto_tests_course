7c0ceaffb4d3621cf7df16668ea323bd6148c248
Моисейкин Павел Николаевич <Moiseykin_PN@gt.local>
2023-06-14 15:18:12 +0500

    update py files

--- conftest.py
+++ conftest.py
@@ -2,17 +2,24 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
+#check parameters for start
 def pytest_addoption(parser):
     parser.addoption('--language', action='store', default='ru',
                      help="Choose language: es or ru")
 
+#fixture for start test
 @pytest.fixture(scope="function")
 def browser(request):
+    #add language
     language = request.config.getoption("language")
-    print("\nStart browser..")
+    
+    #add options for language
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
+    
+    #start browser
     browser = webdriver.Chrome(chrome_options=options)
+    
+    #quit browser
     yield browser
-    print("\nquit browser..")
     browser.quit()
--- test_items.py
+++ test_items.py
@@ -5,11 +5,18 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 
+#link for test
 link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
+#testing link
 def test_chip_basket_button(browser):
+    #open link in browser
     browser.get(link)
+    
+    #time sleep for inspectors
     #time.sleep(30)
+    
+    #check chip basket button
     try: chip_basket_button = WebDriverWait(browser, 3).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'button.btn-add-to-basket')))
     except: chip_basket_button = None
     assert chip_basket_button != None, "Don't have chip basket button!"
